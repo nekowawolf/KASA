5c41ee35a56ae9589c9f0aa91b65015909cc56bf
nekowawolf <irukainu20@gmail.com>
2026-01-23 17:28:15 +0700

    fix: app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'KASA')</title>
-    @vite(entrypoints: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/faq.js'])
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('img/kasa.png') }}">
+
+    <!-- Vite CSS & JS -->
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/faq.js'])
 </head>
 <body class="bg-gray-900 text-white min-h-screen">
 
@@ -13,6 +18,7 @@
     <main>
         @yield('content')
     </main>
+
     @include('components.footer')
 
 </body>
